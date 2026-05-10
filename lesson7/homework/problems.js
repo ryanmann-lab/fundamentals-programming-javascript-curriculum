b53dfc6fc6183761ffa45d81ca45a4dce286fd1c
ryanmann-lab <ryanmann82@gmail.com>
2026-05-10 23:25:40 +0000

    Lesson 7 homework

--- lesson7/homework/problems.js
+++ lesson7/homework/problems.js
@@ -19,4 +19,47 @@
 
 
 // Problem 5
-// Find and print the total sum of all the numbers in the list.
+// Find and print the total sum of all the numbers in the list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("..................")
+console.log("If you didn't know: The lore of FNAF is that William Afton and Henry Emily opened a restaurant around the early 70s, named 'Fredbear's Family Diner', which quickly evolved into a chain of restaurants known as 'Freddy Fazbear's Pizza', started in 1983, under the company of Fazbear Entertainment, Inc. At some point, both of them left the company, with an unknown individual taking over as CEO. William was secretly a serial k*ller, using mascot costumes to lure, abduct, and m**der kids. His motives aren't known, though widely speculated to be him trying to achieve immortality through researching the paranormal, haunted metal (known as remnant), which he created through the m**ders. His first target was Charlotte Emily, the daughter of Henry. She became the Marionette, a ghost who took care of and helped awaken the other lost souls. In 1983, William's younger son also died in an incident, at the fault of his eldest — Michael. The younger son (name unknown, BV for short) 's fate is still unknown, though often speculated to be connected to either Golden Freddy (an enigmatic spirit taking the form of a yellow bear) or helping Charlie with saving other souls. 'I will put you back together' was the last thing said to BV by a debatable party, most commonly believed to be either William, Charlie, or Golden Freddy.In 1985, William k*lled 2 kids, and 3 others were later connected to that incident.")
